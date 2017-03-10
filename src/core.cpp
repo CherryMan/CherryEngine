d938696e5bf97fd28ef73527ce5fde677d4f55c0
CherryMan <skipper308@hotmail.ca>
2017-03-10 07:47:12 -0500

    Add basic files and glad

---
+++ src/core.cpp
@@ -0,0 +1,17 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "core.h"
+
+
+void init() {
+    // TODO
+}
+
+void loop() {
+    // TODO
+}
+
+void end() {
+    // TODO
+}
---
+++ src/core.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Initialize the libs (glfw, opengl, ...)
+void init();
+
+// Main game loop
+void loop();
+
+// Terminate cleanly
+void end();
