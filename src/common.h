a59b8b2fda16ee6f840fe8ef79cc9d3da02b3043
CherryMan <skipper308@hotmail.ca>
2017-03-13 14:17:05 -0400

    Implement core functions

---
+++ src/common.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#include <GLFW/glfw3.h>
+
+
+// Identifier for the glfw window
+GLFWwindow* glfw_window;
--- src/core.cpp
+++ src/core.cpp
@@ -2,16 +2,47 @@
 #include <GLFW/glfw3.h>
 
 #include "core.h"
+#include "common.h"
 
 
 void init() {
-    // TODO
+
+    if (!glfwInit()) {
+        // TODO error logger
+    }
+
+    glfw_window = glfwCreateWindow(640, 480, "title", NULL, NULL);
 }
 
 void loop() {
-    // TODO
+
+    // Ticks per second (note: temporary)
+    const int tickspeed = 25;
+
+    // GLFW uses seconds
+    const double interval = 1 / (double) tickspeed;
+
+    // Loop variables
+    double next_tick = 0;
+
+    // Game loop
+    // Updates according to tickspeed
+    // Renders as fast as possible
+    // Note: interp will be added
+    while (!glfwWindowShouldClose(glfw_window)) {
+
+        while (glfwGetTime() > next_tick) {
+            glfwPollEvents();
+            // TODO update
+
+            next_tick += interval;
+        }
+
+        // TODO render
+
+    }
 }
 
 void end() {
-    // TODO
+    glfwTerminate();
 }
